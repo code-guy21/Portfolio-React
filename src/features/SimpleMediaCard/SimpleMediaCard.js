bc0d9d4981924ad15025ff41ffe5e19f8d57668d
code-guy21 <alexissj16@knights.ucf.edu>
2018-08-27 18:35:29 -0400

    initial commit

--- src/features/SimpleMediaCard/SimpleMediaCard.js
+++ src/features/SimpleMediaCard/SimpleMediaCard.js
@@ -6,8 +6,6 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import Play from '@material-ui/icons/PlayArrow';
-import Download from '@material-ui/icons/FileDownload';
 
 const styles = {
 	media: {
