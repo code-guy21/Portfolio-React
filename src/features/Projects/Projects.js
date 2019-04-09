0f7831fc6cc1d04d22f4874699d5d13329644338
code-guy21 <alexissj16@knights.ucf.edu>
2019-04-08 21:01:13 -0400

    added animation to projects section

--- src/features/Projects/Projects.js
+++ src/features/Projects/Projects.js
@@ -2,24 +2,29 @@ import React from 'react';
 import { Grid } from '@material-ui/core';
 import SimpleMediaCard from '../SimpleMediaCard/SimpleMediaCard';
 import data from './ProjectData';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 const Projects = () => {
 	return (
 		<Grid container justify="center" spacing={16}>
-			{data.map((project, i) => {
-				return (
-					<Grid key={i} item xl={3} lg={4} md={4} sm={10} xs={12}>
-						<SimpleMediaCard
-							webAddress={project.webAddress}
-							learnMore={project.learnMore}
-							photoURL={project.photoURL}
-							title={project.title}
-							description={project.description}
-							github={project.github}
-						/>
-					</Grid>
-				);
-			})}
+			<Stagger in>
+				{data.map((project, i) => {
+					return (
+						<Fade in>
+							<Grid key={i} item xl={3} lg={4} md={4} sm={10} xs={12}>
+								<SimpleMediaCard
+									webAddress={project.webAddress}
+									learnMore={project.learnMore}
+									photoURL={project.photoURL}
+									title={project.title}
+									description={project.description}
+									github={project.github}
+								/>
+							</Grid>
+						</Fade>
+					);
+				})}
+			</Stagger>
 		</Grid>
 	);
 };
