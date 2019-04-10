b60c22edaba770172e354e9229a19d89842c94a7
code-guy21 <alexissj16@knights.ucf.edu>
2019-04-10 08:32:44 -0400

    updated bio

--- src/features/Bio/Bio.js
+++ src/features/Bio/Bio.js
@@ -35,7 +35,7 @@ const Bio = () => {
 					variant="display1"
 					align="center"
 				>
-					A web developer with 2 years of experience in the education,
+					An App developer with 2 years of experience in the education,
 					simulation, and technology industries. An adaptable, self-driven, and
 					communicative problem solver who specializes in front-end development.
 					A 2017 Computer Engineering graduate of the University of Central
