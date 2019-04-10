0b989519dc4f45ab6904e9a2ef30e9d56bca45a2
code-guy21 <alexissj16@knights.ucf.edu>
2019-04-10 08:28:59 -0400

    removed animations

--- src/features/Projects/Projects.js
+++ src/features/Projects/Projects.js
@@ -7,24 +7,20 @@ import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 const Projects = () => {
 	return (
 		<Grid container justify="center" spacing={16}>
-			<Stagger in>
-				{data.map((project, i) => {
-					return (
-						<Fade in>
-							<Grid key={i} item xl={3} lg={4} md={4} sm={10} xs={12}>
-								<SimpleMediaCard
-									webAddress={project.webAddress}
-									learnMore={project.learnMore}
-									photoURL={project.photoURL}
-									title={project.title}
-									description={project.description}
-									github={project.github}
-								/>
-							</Grid>
-						</Fade>
-					);
-				})}
-			</Stagger>
+			{data.map((project, i) => {
+				return (
+					<Grid key={i} item xl={3} lg={4} md={4} sm={10} xs={12}>
+						<SimpleMediaCard
+							webAddress={project.webAddress}
+							learnMore={project.learnMore}
+							photoURL={project.photoURL}
+							title={project.title}
+							description={project.description}
+							github={project.github}
+						/>
+					</Grid>
+				);
+			})}
 		</Grid>
 	);
 };
